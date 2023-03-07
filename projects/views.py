5d304f06649c670ade39af2bec1bbd093e6b4ad0
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-03-07 15:07:54 -0800

    F13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
+from tasks.models import Task
 
 
 @login_required
@@ -8,3 +9,11 @@ def list_projects(request):
     projects = Project.objects.filter(owner=request.user)
     context = {"projects": projects}
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = Project.objects.get(id=id)
+    task = Task.objects.filter(project=project)
+    context = {"project": project, "task": task}
+    return render(request, "projects/detail.html", context)
