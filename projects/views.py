1f582fee3aa152f242ae452b308243ce438e2820
Adrianna Helfrich <adriannahelfrich99@gmail.com>
2023-03-07 17:42:38 -0800

    F14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
 from tasks.models import Task
+from projects.forms import CreateProjectForm
 
 
 @login_required
@@ -17,3 +18,20 @@ def show_project(request, id):
     task = Task.objects.filter(project=project)
     context = {"project": project, "task": task}
     return render(request, "projects/detail.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = CreateProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(False)
+            project.owner = request.user
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = CreateProjectForm()
+
+    context = {"form": form}
+
+    return render(request, "projects/create.html", context)
